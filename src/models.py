24efe361ec79e3483de90d19095631fea871996c
FrankLobo <frankeloboq@gmail.com>
2022-02-21 21:04:01 +0000

    actualizando datos

--- src/models.py
+++ src/models.py
@@ -51,22 +51,57 @@ class Planets(Base):
     __tablename__ = 'planets'
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+    population = Column(String(250), nullable=False)
+    rotaion_period = Column(String(250), nullable=False)
+    orbital_period = Column(String(250), nullable=False)
+    diameter = Column(String(250), nullable=False)
+    gravity = Column(String(250), nullable=False)
+    terrain = Column(String(250), nullable=False)
+    climate = Column(String(250), nullable=False)
     
-
 class Characters(Base):
     __tablename__ = 'characters'
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+    birth = Column(String(250), nullable=False)
+    species = Column(String(250), nullable=False)
+    height = Column(String(250), nullable=False)
+    mass = Column(String(250), nullable=False)
+    gender = Column(String(250), nullable=False)
+    hair_color = Column(String(250), nullable=False)
+    skin_color = Column(String(250), nullable=False)
+    home_world = Column(String(250), nullable=False)
 
 class Vehicles(Base):
     __tablename__ = 'vehicles'
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+    model = Column(String(250), nullable=False)
+    manufacture = Column(String(250), nullable=False)
+    class_type = Column(String(250), nullable=False)
+    speed = Column(String(250), nullable=False)
+    cost = Column(String(250), nullable=False)
+    lenght = Column(String(250), nullable=False)
+    cargo_capacity = Column(String(250), nullable=False)
+    minimum_crew = Column(String(250), nullable=False)
+    passanger = Column(String(250), nullable=False)
+
 
 class Starships(Base):
     __tablename__ = 'starships'
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+    model = Column(String(250), nullable=False)
+    manufacture = Column(String(250), nullable=False)
+    class_type = Column(String(250), nullable=False)
+    speed = Column(String(250), nullable=False)
+    cost = Column(String(250), nullable=False)
+    hyperdrive_rating = Column(String(250), nullable=False)
+    mglt = Column(String(250), nullable=False)
+    lenght = Column(String(250), nullable=False)
+    cargo_capacity = Column(String(250), nullable=False)
+    minimum_crew = Column(String(250), nullable=False)
+    passanger = Column(String(250), nullable=False)
     
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
